c56b4c50b4721b66e67f8de9e23b5753bb60f18b
Drew Wells <drew.wells00@gmail.com>
2016-03-04 20:33:07 -0600

    started doing weird stuff like starting ocproxy, maybe useful one day

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"os/exec"
 	"os/signal"
+	"time"
 
 	"github.com/drewwells/proxy"
 )
@@ -25,10 +27,45 @@ func main() {
 	}
 
 	f := os.NewFile(cfg.VPNFD, "mysocket")
+	go func() {
+		return
+		l, err := net.FileListener(f)
+		fmt.Println("opened file listener", l)
+		if err != nil {
+			fmt.Println("failed to retrieve listener", err)
+			return
+		}
+
+		for {
+			fmt.Println("waiting to accept")
+			// conn, err := l.Accept()
+			fmt.Println(l.Addr())
+			fmt.Printf("addr % #v\n", l.Addr())
+			conn, err := net.ListenUnixgram("unixgram", l.Addr().(*net.UnixAddr))
+			fmt.Println("accepted conn")
+			if err != nil {
+				fmt.Println("err accepting conn", err)
+				return
+			}
+			bs := make([]byte, 512)
+			fmt.Println("reading from conn")
+			n, err := conn.Read(bs)
+			if err != nil {
+				fmt.Println("err reading conn", err)
+				continue
+			}
+			fmt.Println("read", n, "bytes")
+			fmt.Println(string(bs[:n]))
+		}
+	}()
+	// time.Sleep(1 * time.Minute)
+	// return
 	upstream, err := net.FileConn(f)
 	if err != nil {
 		// log.Fatal(err)
 	}
+	// uf, _ := upstream.(*net.UnixConn).File()
+	// fmt.Printf("% #v\n", pretty.Formatter(uf))
 
 	if _, err = os.Stat(socks.PATH); err == nil {
 		os.Remove(socks.PATH)
@@ -39,105 +76,78 @@ func main() {
 		fmt.Println("removed", socks.PATH)
 	}()
 
-	l, err := net.Listen("unix", socks.PATH)
+	addr, err := net.ResolveUnixAddr("unixgram", socks.PATH)
+	conn, err := net.ListenUnixgram("unixgram", addr)
 	if err != nil {
 		log.Fatal(err)
 	}
+	go func() {
+		for {
 
+			b, obb := make([]byte, 512), make([]byte, 512)
+			n, oobn, flags, addr, err := conn.ReadMsgUnix(b, obb)
+			fmt.Println(n, oobn, flags, addr, err)
+
+		}
+	}()
+	ff, err := conn.File()
+	if err != nil {
+		log.Fatal(err)
+	}
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	go func() {
 		for sig := range c {
 			_ = sig
 			fmt.Println("shutdown requested")
-			l.Close()
+			log.Fatal("done")
 		}
 	}()
 
 	fmt.Println("listening on", socks.PATH)
+	if true {
+		go func() {
+			s := "/usr/local/bin/ocproxy"
+			cmd := exec.Command(s, "-D 11080", "-v")
+			cmd.Env = []string{
+				fmt.Sprintf("VPNFD=%d", ff.Fd()),
+				"INTERNAL_IP4_DNS=144.20.190.70",
+				"INTERNAL_IP4_ADDRESS=10.154.167.196",
+				"INTERNAL_IP4_MTU=1300",
+			}
+			cmd.ExtraFiles = []*os.File{ff}
+			fmt.Println("staring with", ff.Fd())
+			bs, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println("cmd output", string(bs))
+		}()
+	}
+	time.Sleep(1 * time.Minute)
+	return
+	l, err := net.Listen("unix", socks.PATH)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ul := l.(*net.UnixListener)
+
 	// Start socks proxy server. Accepting incoming requests
 	// to forward to the vpnfd sock
-	serve(upstream, l)
+	serve(upstream, ul)
 
-	// in, err := net.Listen("unix", path)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer fmt.Println("closing socket")
-	// defer in.Close()
-	// fmt.Printf("% #v\n", in)
-
-	// ss := os.Environ()
-	// for _, s := range ss {
-	// 	fmt.Println(s)
-	// }
-
-	// log.Printf("conn % #v\n", conn)
-	// n, err := conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
-	// log.Println("write", n, err)
-
-	// go func() {
-	// 	for {
-	// 		result, err := ioutil.ReadAll(conn)
-	// 		log.Println("readall", err, result)
-	// 	}
-	// }()
-
-	// laddr, err := net.ResolveUnixAddr("unix", "")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// raddr, err := net.ResolveUnixAddr("unix", path)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// conn, err := net.DialUnix("unix", laddr, raddr)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// fmt.Println("getting fd", vpnfd)
-	// f, err := fd.Get(conn, vpnfd, []string{"duh"})
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("f[0] % #v\n", f[0])
-	// names, err := f[0].Readdirnames(9)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println("names", names)
-	// _, err = f[0].WriteString("Stuff happening now")
-	// fmt.Println(err)
-	// // f := os.NewFile(vpnfd, "tunfile")
-	// fmt.Printf("% #v\n", f)
-	// log.Println("VPNFD:", vpnfd)
-	// log.Fatal(os.Args)
 }
 
-func listenAndWrite(upstream, in net.Conn) {
+func listenAndWrite(upstream net.Conn, in *net.UnixConn) {
 	bs := make([]byte, 512)
 	fmt.Println("waiting to read")
-	var readsomething bool
-	for {
-		n, err := in.Read(bs)
-		if err != nil {
-			break
-		}
-		if n > 0 {
-			fmt.Printf("found %d bytes\n", n)
-			fmt.Println(string(bs))
-			readsomething = true
-			_, err := upstream.Write(bs[:n])
-			if err != nil {
-				fmt.Println("error writing to upstream",
-					err)
-			}
-		}
-	}
-	if readsomething {
+	var wrotesomething bool
+	_ = wrotesomething
+	go func() {
+		fmt.Println("read routine")
 		var ups []byte
 		for {
+			fmt.Println("reading from upstream")
 			n, err := upstream.Read(ups)
 			if err != nil {
 				fmt.Println("error reading from upstream", err)
@@ -147,19 +157,43 @@ func listenAndWrite(upstream, in net.Conn) {
 				fmt.Println("reading 0 bytes")
 				break
 			}
-			fmt.Printf("reading %d bytes: err\n", n, err)
+			fmt.Printf("read %d bytes: err\n", n, err)
+		}
+	}()
+
+	for {
+		n, err := in.Read(bs)
+		if err != nil {
+			fmt.Println("in read err, breaking", err)
+			break
+		}
+		if n == 0 {
+			fmt.Println("read nothing breaking")
+			break
+		}
+		if n > 0 {
+			fmt.Printf("found %d bytes\n", n)
+			fmt.Println("writing to upstream")
+			wrotesomething = true
+			nn, err := upstream.Write(bs[:n])
+			if err != nil {
+				fmt.Println("error writing to upstream",
+					err)
+			}
+			fmt.Println("wrote", nn, "bytes")
 		}
 	}
 }
 
-func serve(upstream net.Conn, l net.Listener) {
+func serve(upstream net.Conn, l *net.UnixListener) {
 	for {
 		fmt.Println("waiting to accept")
-		conn, err := l.Accept()
+		conn, err := l.AcceptUnix()
 		if err != nil {
 			fmt.Println("error accepting conn", err)
 			break
 		}
+		fmt.Println("accepted conn", conn)
 		// these leak
 		go listenAndWrite(upstream, conn)
 	}
